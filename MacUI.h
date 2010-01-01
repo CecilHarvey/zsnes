7395a6d63881604cf09abc6a2fa46cba43e91e50
xenoveritas <xenoveritas@94d0a883-1e7c-0410-a1ba-e24ec5ee78a7>
2010-01-01 21:47:21 +0000

    File cleanup, fixed the About dialog so that launching browser links works now

--- MacUI.h
+++
@@ -1,9 +0,0 @@
-//
-//  MacUI.h
-//  zsnes
-//
-//  Created by Daniel Potter on 12/30/09.
-//  Helper functions for displaying errors from C code.
-//
-
-void macos_alert(char* format, ...);
--- macosx/AboutDialog.h
+++ macosx/AboutDialog.h
@@ -10,9 +10,21 @@
 
 @interface AboutDialog : NSWindowController {
 	IBOutlet NSTextField* versionLabel;
+	IBOutlet NSTextView* linkTextView;
 }
 
 -(id) init;
 -(void) windowDidLoad;
 
 @end
+
+@interface ZAboutWindow : NSWindow {
+	IBOutlet NSView* normalView;
+	IBOutlet NSView* eeggView;
+	BOOL eegg;
+}
+
+- (void)keyDown:(NSEvent *)event;
+
+@end
+
